00bcbe36d97dbbf0ac876d279adb960b2ba81e33
jp956956 <jp956956@gmail.com>
2020-11-10 13:48:27 +0800

    2

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    // 購物車
+    items: []
+  },
+  mutations: {
+    // state 代表上面的 state
+    // data 代表傳入的吃資料
+    addCart (state, data) {
+      state.items.push(data)
+    },
+    delCart (state, index) {
+      state.items.splice(index, 1)
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
---
+++ src/views/Cart.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  #cart
+    b-table(:items="items" :fields="fields" stripted hover)
+      template(#cell(action)="data")
+        b-button(variant="danger" @click="del(data.index)")
+          font-awesome-icon(:icon="['far', 'trash-alt']")
+</template>
+
+<script>
+export default {
+  name: 'Cart',
+  data () {
+    return {
+      fields: [
+        {
+          key: 'name',
+          label: '商品名稱'
+        },
+        {
+          key: 'price',
+          label: '價格'
+        },
+        {
+          key: 'action',
+          label: '操作'
+        }
+      ]
+    }
+  },
+  computed: {
+    items () {
+      return this.$store.state.items
+    }
+  },
+  methods: {
+    del (index) {
+      this.$store.commit('delCart', index)
+    }
+  }
+}
+</script>
